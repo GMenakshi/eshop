67e9f25956b0ff823c0887f91f6223395f6a4e65
G MENAKSHI <kshimeena437@gmail.com>
2026-09-25 14:46:45 +0530

    New changes

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -14,6 +14,7 @@ import { VideoView, useVideoPlayer } from "expo-video";
 import { useEffect, useState } from "react";
 import {
   Animated,
+  Modal,
   type NativeScrollEvent,
   type NativeSyntheticEvent,
   Pressable,
